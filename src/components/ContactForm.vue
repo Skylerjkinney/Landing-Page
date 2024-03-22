0ff036681dedfd20fc1f4520dc28b8fd9e55907b
skyler kinney <skylerjkinney@gmail.com>
2024-03-22 17:58:10 -0600

    force node update, check to see if breaks

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -38,6 +38,7 @@ export default {
     return {
       editable,
       handleSubmit() {
+
         // For a free Serverless option consider a google form 
         // follow this tutorial https://blog.webjeda.com/google-form-customize/ 
         // Easy Mode - https://stefano.brilli.me/google-forms-html-exporter/
