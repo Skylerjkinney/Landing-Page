73b20378a4a4c097641fc76a83dc65a31297d4f1
skyler kinney <skylerjkinney@gmail.com>
2024-04-03 13:27:57 -0600

    working on nodemailer

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -27,8 +27,18 @@
 <script>
 import { ref } from 'vue';
 
+
 export default {
   setup() {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
     const editable = ref({
       name: '',
       email: '',
@@ -37,7 +47,15 @@ export default {
 
     return {
       editable,
-      handleSubmit() {
+      async handleSubmit() {
+        const info = await transporter.sendMail({
+          from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+          to: "bar@example.com, baz@example.com", // list of receivers
+          subject: "Hello ✔", // Subject line
+          text: "Hello world?", // plain text body
+          html: "<b>Hello world?</b>", // html body
+        });
+        console.log("Message sent: %s", info.messageId);
 
         // For a free Serverless option consider a google form 
         // follow this tutorial https://blog.webjeda.com/google-form-customize/ 
