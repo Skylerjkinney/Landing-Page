99d37405325764522551dd083f132905fe9ff6d0
skyler kinney <skylerjkinney@gmail.com>
2024-04-03 14:27:27 -0600

    contact works?

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,20 +5,45 @@
       <div class="fs-4 fw-bold mb-3">
         Let's connect!
       </div>
-      <form class="contact-form" @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input class="form-control" type="text" id="name" v-model="editable.name" required>
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input class="form-control" type="email" id="email" v-model="editable.email" required>
-        </div>
-        <div class="form-group">
-          <label for="message">Message</label>
-          <textarea class="form-control" id="message" v-model="editable.message" required></textarea>
-        </div>
-        <button class="btn btn-primary" type="submit">Submit</button>
+      <form
+        action="https://docs.google.com/forms/d/e/1FAIpQLSdkhxKUuGmmD50Hsi8GtZDLucyezfXAOp2RMHJw7QxIRRG8hA/formResponse"
+        target="_self" id="bootstrapForm" method="POST">
+        <fieldset>
+          <h2>Contact Information<br><small></small></h2>
+        </fieldset>
+
+
+
+        <fieldset>
+          <legend for="1633920210">Name</legend>
+          <div class="form-group">
+            <input id="2005620554" type="text" name="entry.2005620554" class="form-control" required>
+          </div>
+        </fieldset>
+
+
+
+        <fieldset>
+          <legend for="227649005">Email</legend>
+          <div class="form-group">
+            <input id="1045781291" type="text" name="entry.1045781291" class="form-control" required>
+          </div>
+        </fieldset>
+
+
+
+        <fieldset>
+          <legend for="1846923513">Comments</legend>
+          <div class="form-group">
+            <textarea id="839337160" name="entry.839337160" class="form-control"></textarea>
+          </div>
+        </fieldset>
+
+        <input type="hidden" name="fvv" value="1">
+        <input type="hidden" name="fbzx" value="-7425823956835125764">
+        <input type="hidden" name="pageHistory" value="0">
+
+        <input class="btn btn-primary" type="submit" value="Submit">
       </form>
     </div>
   </div>
